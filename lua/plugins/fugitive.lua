4e73c422394cc7c3c5c7c13a91adfdc322e09167
Willy <guillecg13@gmail.com>
2025-08-28 15:47:56 +0000

    feat: add fugitive git plugin

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,9 @@
 return {
-	{"tpope/vim-fugitive",}
+	{"tpope/vim-fugitive",
+	cmd = {"G", "Git"},
+	keys = {
+		{"<leader>gf", ":Git fetch --all<cr>", desc = "Git fetch"},
+		{"<leader>gl", ":Git pull<cr>", desc = "Git pull"},
+	},
+	},
 }
