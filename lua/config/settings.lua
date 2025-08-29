3eab33d17e16bd6578eb8c143d7974e59ded329a
Willy <guillecg13@gmail.com>
2025-08-29 16:31:08 +0000

    feat: set tab indention to 2

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,2 +1,13 @@
-vim.o.number = true
+-- Leader Key
 vim.g.mapleader = " "
+
+-- Line numbers
+vim.o.number = true
+
+-- Indentation
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.expandtab = false
